e7aadb78d8ad5460766b5a4fe50dda2208cf41ca
ronney221 <ronneydo1@gmail.com>
2025-07-03 22:57:23 -0700

    added reactbits and brand colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+
+
 export default function App() {
+
+    
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <p className="text-2xl font-bold">Hello World</p>
+
+      
     </div>
   )
 }
