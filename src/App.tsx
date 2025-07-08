1ed5f71d38232c13fd9a2404ac33ddc761936c83
ronney221 <ronneydo1@gmail.com>
2025-07-07 23:13:03 -0700

    new logo svgs with color

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { FAQ } from "./components/sections/FAQ";
 import { Footer } from "./components/layout/Footer";
 import { Hero } from "./components/sections/Hero";
 import { HowItWorks } from "./components/sections/HowItWorks";
+import { PartnerLogos } from "./components/sections/PartnerLogos";
 import { Scrollytelling } from "./components/sections/Scrollytelling";
 import { TheProblem } from "./components/sections/TheProblem";
 import { RootLayout } from "./components/layout/RootLayout";
@@ -12,6 +13,7 @@ function App() {
   return (
     <RootLayout>
       <Hero />
+      <PartnerLogos />
       <TheProblem />
       <CardBenefits />
       <Testimonials />
