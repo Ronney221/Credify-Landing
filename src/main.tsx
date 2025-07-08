1c4a4ad736a0d5b0ac364e050379fb2b0160b6fa
ronney221 <ronneydo1@gmail.com>
2025-07-08 16:15:50 -0700

    analytics for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 import App from "./App.tsx";
 import PrivacyPage from "./pages/privacy.tsx";
+import FeedbackPage from "./pages/feedback.tsx";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -14,10 +16,15 @@ const router = createBrowserRouter([
     path: "/privacy",
     element: <PrivacyPage />,
   },
+  {
+    path: "/feedback",
+    element: <FeedbackPage />,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Analytics />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
